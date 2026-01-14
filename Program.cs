6761bc9ee049fe57dade2f685cd79542d34dce27
Miraj-cybe <mondolm619@gmail.com>
2026-01-14 19:55:01 +0600

    About Constructor

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace practice
+{
+    class Car
+    {
+string CarName ;
+int makeYear;
+//Default Constructor
+public Car()
+        {
+           CarName = "Toyota";
+              makeYear = 2020; 
+        }
+//paramterized Constructor
+public Car( string a ,int x)
+        {
+            CarName =a;
+            makeYear =x;
+        }
+public void displayInfo()
+        {
+            Console.WriteLine("Car Details: " + CarName + "." + " Make Year:  " + makeYear);
+        }
+        static void Main(string[] args)
+        {
+            Car carDetails = new Car();
+          carDetails.displayInfo();
+         
+          Car Cardetails =new Car("Honda",2022);
+          Cardetails.displayInfo();
+        }
+    }
+}
