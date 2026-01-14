32aec2c0a74dd654e907f7961509330bd2d25198
Miraj-cybe <mondolm619@gmail.com>
2026-01-14 21:12:03 +0600

    updated Propersties

--- Program.cs
+++ Program.cs
@@ -2,30 +2,46 @@
 namespace practice
 {
     class Person
-    {
-        public string name = "Miraj Mondol";
+    {       private string name;
         private int age;
 
+        public string Name
+        {
+            get{return name; }
+
+            set
+            {name = value;}
+
+             }
+public int Age
+        {
+            get{ return age;}
+set{ age = value;}
+        }
+
+        }
+
     
-    public void setage(int personAge)  //to read private age variable
+   /*  public void setage(int personAge)  //to write private age variable
         {
             age = personAge;
         }
-//For Readable
+            //For Readable
  public int Getage()
         {
         return age;
         }
+    */
 
-
-    }
+    
     class Program
     {
         static void Main(string[] args)
         {
          Person person1 = new Person();
-         person1.setage(22);
-         Console.WriteLine($"Name: {person1.name} , Age: {person1.Getage()}");
+         person1.Name ="Miraj Mondol";
+         person1.Age = 22;
+         Console.WriteLine($"Name: {person1.Name} , Age: {person1.Age}");
 
         }
     }
